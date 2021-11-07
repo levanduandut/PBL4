eeec51db14ce0be84a18ee349caa7d65b12fd4d2
levanduandut <77284378+levanduandut@users.noreply.github.com>
2021-11-07 21:39:56 +0700

    ii

--- PBL4/MyPing.h
+++ PBL4/MyPing.h
@@ -34,6 +34,14 @@ namespace PBL4 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Button^ btnAddHost;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::DataGridView^ dtgvHosts;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,20 +56,94 @@ namespace PBL4 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->btnAddHost = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->dtgvHosts = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dtgvHosts))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(64, 16);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Add Host";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(13, 38);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(33, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Host";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(82, 35);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(524, 21);
+			this->comboBox1->TabIndex = 2;
+			// 
+			// btnAddHost
+			// 
+			this->btnAddHost->Location = System::Drawing::Point(632, 33);
+			this->btnAddHost->Name = L"btnAddHost";
+			this->btnAddHost->Size = System::Drawing::Size(146, 23);
+			this->btnAddHost->TabIndex = 3;
+			this->btnAddHost->Text = L"PING";
+			this->btnAddHost->UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(15, 78);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(51, 20);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Hosts";
+			// 
+			// dtgvHosts
+			// 
+			this->dtgvHosts->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dtgvHosts->Location = System::Drawing::Point(12, 112);
+			this->dtgvHosts->Name = L"dtgvHosts";
+			this->dtgvHosts->Size = System::Drawing::Size(780, 248);
+			this->dtgvHosts->TabIndex = 5;
 			// 
 			// MyPing
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::Fuchsia;
-			this->ClientSize = System::Drawing::Size(424, 261);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->ClientSize = System::Drawing::Size(804, 381);
+			this->Controls->Add(this->dtgvHosts);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->btnAddHost);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->ForeColor = System::Drawing::SystemColors::ControlText;
 			this->Name = L"MyPing";
 			this->Text = L"MyPing";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dtgvHosts))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+};
 }
