4485b6501ba5334efdb06cfb9158d0f166f0e4b3
levanduandut <77284378+levanduandut@users.noreply.github.com>
2021-11-11 11:03:55 +0700

    done

--- PBL4/MyTracert.h
+++ PBL4/MyTracert.h
@@ -35,6 +35,21 @@ namespace PBL4 {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
 	protected:
 
 	private:
@@ -51,6 +66,22 @@ namespace PBL4 {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -60,9 +91,134 @@ namespace PBL4 {
 				static_cast<System::Byte>(0)));
 			this->label1->Location = System::Drawing::Point(146, 107);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(397, 55);
+			this->label1->Size = System::Drawing::Size(0, 55);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Chưa biết thiết kế";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(22, 24);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(40, 16);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"IPV :";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"4", L"6" });
+			this->comboBox1->Location = System::Drawing::Point(79, 19);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(67, 21);
+			this->comboBox1->TabIndex = 2;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(177, 24);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(76, 16);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"TTL Max :";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(259, 20);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 4;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(380, 24);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(66, 16);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L"TimeOut";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(476, 19);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(64, 20);
+			this->textBox2->TabIndex = 6;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(569, 24);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(58, 16);
+			this->label5->TabIndex = 7;
+			this->label5->Text = L"Reolve";
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Yes", L"No" });
+			this->comboBox2->Location = System::Drawing::Point(655, 19);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(67, 21);
+			this->comboBox2->TabIndex = 8;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(153, 64);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(120, 16);
+			this->label6->TabIndex = 9;
+			this->label6->Text = L"Host or Address";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(299, 63);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(423, 20);
+			this->textBox3->TabIndex = 10;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
+				this->Column1,
+					this->Column2, this->Column3, this->Column4
+			});
+			this->dataGridView1->Location = System::Drawing::Point(25, 117);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(757, 252);
+			this->dataGridView1->TabIndex = 11;
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"TTL";
+			this->Column1->Name = L"Column1";
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"Time 1";
+			this->Column2->Name = L"Column2";
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"Time 2";
+			this->Column3->Name = L"Column3";
+			// 
+			// Column4
+			// 
+			this->Column4->HeaderText = L"Time3";
+			this->Column4->Name = L"Column4";
 			// 
 			// MyTracert
 			// 
@@ -71,13 +227,25 @@ namespace PBL4 {
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
 				static_cast<System::Int32>(static_cast<System::Byte>(0)));
 			this->ClientSize = System::Drawing::Size(804, 381);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"MyTracert";
 			this->Text = L"MyTracert";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+};
 }
